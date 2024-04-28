5c3d482fbd400be8d21283799d1729d9efaa650d
00197mfz <golammafuzgm@gmail.com>
2024-04-28 16:11:01 +0530

    Implemented Navigation, Login, Register UI and connected backend to frontend using RTK Query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,26 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
-import { createBrowserRouter } from "react-router-dom";
 
+// Auth
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={ <App /> } />
+    <Route path="/" element={ <App /> }>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Route>
   )
 );
 
-ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
-  < RouterProvider router={router}/>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+      <RouterProvider router={router} />
+  </Provider>
 );
