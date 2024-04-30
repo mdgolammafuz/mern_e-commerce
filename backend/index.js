d22f747f48aec9333df51ff1763c3d4d077e7dcd
00197mfz <golammafuzgm@gmail.com>
2024-04-30 16:27:29 +0530

    Implemented API for creating Product Category by Admin

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import { config } from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { connectToDatabase } from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
+import categoryRoutes from "./routes/categoryRoutes.js";
 config()
 const port = process.env.PORT || 5000
 
@@ -15,5 +16,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use( cookieParser() );
 
 app.use( "/api/users", userRoutes);
-
+app.use("/api/category", categoryRoutes);
 app.listen(port, () => console.log(`Server running on port: ${port}`));
