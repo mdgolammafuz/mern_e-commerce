0181a18ee8e049074147104fab20017a0679b602
00197mfz <golammafuzgm@gmail.com>
2024-05-04 17:20:15 +0530

    Get a product by Id and get all products API implemented

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -78,8 +78,69 @@ const removeProduct = asyncHandler(async (req, res) => {
   }
 } );
 
+
+const fetchProducts = asyncHandler(async (req, res) => {
+  try {
+    const pageSize = 6;
+
+    const keyword = req.query.keyword
+      ? {
+          name: {
+            $regex: req.query.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize);
+
+    res.json({
+      products,
+      page: 1,
+      pages: Math.ceil(count / pageSize),
+      hasMore: false,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+const fetchProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ error: "Product not found" });
+  }
+});
+
+const fetchAllProducts = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find({})
+      .populate("category")
+      .limit(12)
+      .sort({ createAt: -1 });
+
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
 export {
   addProduct,
   updateProductDetails,
   removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,17 +6,23 @@ import {
   addProduct,
   updateProductDetails,
   removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
 } from "../controllers/productController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 router
   .route("/")
+  .get(fetchProducts)
   .post(authenticate, authorizeAdmin, formidable(), addProduct);
 
+router.route("/allproducts").get(fetchAllProducts);
 
 router
   .route("/:id")
+  .get(fetchProductById)
   .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
-  .delete( authenticate, authorizeAdmin, removeProduct );
-  
+  .delete(authenticate, authorizeAdmin, removeProduct);
+
 export default router;
