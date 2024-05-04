a1f260b11c9b30d08adf21ea77e1f82b507a5c15
00197mfz <golammafuzgm@gmail.com>
2024-05-05 00:36:54 +0530

    Get Top products and Newest products API implemented

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,8 @@ import {
   fetchProductById,
   fetchAllProducts,
   addProductReview,
+  fetchTopProducts,
+  fetchNewProducts,
 } from "../controllers/productController.js";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js";
@@ -21,6 +23,10 @@ router
 
 router.route("/allproducts").get(fetchAllProducts);
 router.route("/:id/reviews").post(authenticate, checkId, addProductReview);
+
+router.get("/top", fetchTopProducts);
+router.get("/new", fetchNewProducts);
+
 router
   .route("/:id")
   .get(fetchProductById)
