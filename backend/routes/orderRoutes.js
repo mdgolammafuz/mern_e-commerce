a2623dfd1f446a22dbc7795240258b09b4f159e6
00197mfz <golammafuzgm@gmail.com>
2024-05-14 23:38:38 +0530

    Create Order API implemented

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+
+import {
+  createOrder,
+} from "../controllers/orderController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+router
+  .route("/")
+  .post(authenticate, createOrder)
+  
+export default router;
