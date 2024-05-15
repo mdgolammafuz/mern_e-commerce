aed0ff418dbcbb2581bd19d37d67a68d4e651580
00197mfz <golammafuzgm@gmail.com>
2024-05-15 18:36:32 +0530

    Admin action: Mark Order as delivered API implemented

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -9,6 +9,8 @@ import {
   calculateTotalSales,
   calcualteTotalSalesByDate,
   findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
 } from "../controllers/orderController.js";
 
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
@@ -23,5 +25,10 @@ router.route("/total-orders").get(countTotalOrders);
 router.route("/total-sales").get(calculateTotalSales);
 router.route("/total-sales-by-date").get(calcualteTotalSalesByDate);
 router.route("/:id").get(authenticate, findOrderById);
+router.route("/:id/pay").put(authenticate, markOrderAsPaid);
+router
+  .route("/:id/deliver")
+  .put(authenticate, authorizeAdmin, markOrderAsDelivered);
+
 
 export default router;
